558eb47af0ec3f14a772ac892524e7564c3bbed6
Brandon Stiles <brandonstiles@Brandons-MacBook-Pro.local>
2017-12-18 15:28:32 -0700

    server process.env fix

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ mongoose.connect(db, err => {
     }
 })
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(bodyParser.json());
 app.use(express.static(__dirname + "/build"));
