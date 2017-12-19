c7de400c8215c388dceeeaed441420895add1459
Brandon Stiles <brandonstiles@Brandons-MacBook-Pro.local>
2017-12-19 11:31:44 -0700

    added standings table

--- src/components/Standings.js
+++ src/components/Standings.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import BarChart from './BarChart';
+import StandingsTable from './StandingsTable';
 
 export default class Home extends React.Component {
     render() {
         return (
             <div id='standings'>
-                <div className='standings-child'>
+                <div className='standings-charts'>
                     <BarChart stats={this.props.stats} sort='top' />
                     <BarChart stats={this.props.stats} sort='bottom' />
                 </div>
-                <div className='standings-child'>
-
+                <div className='standings-table'>
+                    <StandingsTable stats={this.props.stats} />
                 </div>
             </div>
         )
