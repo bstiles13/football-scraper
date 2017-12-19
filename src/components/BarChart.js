1ff023d0bf59c8a6252bcb2b9388b5e9176f2610
Brandon Stiles <brandonstiles@Brandons-MacBook-Pro.local>
2017-12-19 13:04:49 -0700

    home header

--- src/components/BarChart.js
+++ src/components/BarChart.js
@@ -3,74 +3,82 @@ import { Chart } from 'chart.js';
 
 export default class BarChart extends React.Component {
 
+    componentDidMount() {
+        { this.props.stats ? this.buildChart() : false };
+    }
+
     componentDidUpdate(prevProps) {
         if (this.props.stats != prevProps.stats) {
-            let backgroundColor = '';
-            let borderColor = '';
-            let label = '';                
-            let standings = this.props.stats;
-            if (this.props.sort == 'top') {
-                console.log('ascending');
-                backgroundColor = 'rgba(54, 162, 235, 0.2)';
-                borderColor = 'rgba(54, 162, 235, 1)';
-                label = 'Top Wins'
-                standings.sort((a, b) => {
-                    return b.wins - a.wins;
-                })
-            }
-            if (this.props.sort == 'bottom') {
-                console.log('descending');                
-                backgroundColor = 'rgba(255, 99, 132, 0.2)';
-                borderColor = 'rgba(255, 99, 132, 1)';
-                label = 'Top Losses'                
-                standings.sort((a, b) => {
-                    return a.wins - b.wins;
-                })
-            }
-            standings = standings.slice(0, 10);            
-            let labels = standings.map(team => {
-                return team.team;
-            });
-            console.log(labels);
-            let wins = standings.map(team => {
-                return team.wins;
-            });
-            var ctx = document.getElementById('barchart-' + this.props.sort).getContext('2d');
-            var myChart = new Chart(ctx, {
-                type: 'bar',
-                data: {
-                    labels: labels,
-                    datasets: [{
-                        label: label,
-                        data: wins,
-                        backgroundColor: backgroundColor,
-                        borderColor: borderColor,
-                        borderWidth: 1
-                    }]
-                },
-                options: {
-                    scales: {
-                        yAxes: [{
-                            ticks: {
-                                beginAtZero: true
-                            }
-                        }],
-                        xAxes: [{
-                            ticks: {
-                                autoSkip: false
-                            }
-                        }],
-                    },
-                    maintainAspectRatio: false
-                }
-            });
+            this.buildChart();
         }
+    }
+
+    buildChart() {
+        let backgroundColor = '';
+        let borderColor = '';
+        let label = '';
+        let standings = this.props.stats;
+        if (this.props.sort == 'top') {
+            console.log('ascending');
+            backgroundColor = 'rgba(54, 162, 235, 0.2)';
+            borderColor = 'rgba(54, 162, 235, 1)';
+            label = 'Top Wins'
+            standings.sort((a, b) => {
+                return b.wins - a.wins;
+            })
+        }
+        if (this.props.sort == 'bottom') {
+            console.log('descending');
+            backgroundColor = 'rgba(255, 99, 132, 0.2)';
+            borderColor = 'rgba(255, 99, 132, 1)';
+            label = 'Top Losses'
+            standings.sort((a, b) => {
+                return a.wins - b.wins;
+            })
+        }
+        standings = standings.slice(0, 10);
+        let labels = standings.map(team => {
+            return team.team;
+        });
+        console.log(labels);
+        let wins = standings.map(team => {
+            return team.wins;
+        });
+        var ctx = document.getElementById('barchart-' + this.props.sort).getContext('2d');
+        var myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: labels,
+                datasets: [{
+                    label: label,
+                    data: wins,
+                    backgroundColor: backgroundColor,
+                    borderColor: borderColor,
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }],
+                    xAxes: [{
+                        ticks: {
+                            autoSkip: false
+                        }
+                    }],
+                },
+                maintainAspectRatio: false
+            }
+        });
     }
 
     render() {
         return (
             <div>
-                <canvas id={'barchart-' + this.props.sort} style={{width: '400px', height: '400px'}}></canvas>
+                <canvas id={'barchart-' + this.props.sort} style={{ width: '400px', height: '400px' }}></canvas>
             </div>
         )
     }
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,9 +3,27 @@ import Standings from './Standings';
 import Preview from './Preview';
 
 export default class Home extends React.Component {
+
+    handleClick() {
+        console.log('works');
+    }
+
     render() {
         return (
             <div id='home'>
+                <div className='header'>
+                    <div className='header-top'>
+                        <h3>NFL Overview - 2017</h3>
+                        <div className='header-buttons'>
+                        <button type="button" className="btn btn-sm btn-outline-primary" onClick={this.handleClick.bind(this)}>Division</button>
+                        <button type="button" className="btn btn-sm btn-outline-primary" onClick={this.handleClick.bind(this)}>Conference</button>
+                        <button type="button" className="btn btn-sm btn-outline-primary" onClick={this.handleClick.bind(this)}>League</button>
+                        </div>
+                    </div>
+                    <div className='header-border-one'></div>
+                    <div className='header-border-two'></div>
+                    <div className='header-border-three'></div>
+                </div>
                 <Standings stats={this.props.stats.standings} />
                 <div className='home-row'>
                     <Preview stats={this.props.stats.passing} category='passing' preview='yards' />
